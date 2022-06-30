3f6185bd5f66cb2513dafcb709facf5b200a2111
Felipe Ribeiro <felipe.ribeiro@friday.ai>
2022-06-30 19:33:44 -0300

    feat: ajustes

--- src/functions/deleteTodo/handler.ts
+++ src/functions/deleteTodo/handler.ts
@@ -1,18 +1,18 @@
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
-import { Any } from 'json-schema-to-ts/lib/meta-types';
 import TodoService from 'src/services/TodoService';
 
 const todoService = new TodoService()
 
-const deleteTodo: ValidatedEventAPIGatewayProxyEvent<Any> = async (event) => {
+const deleteTodo: ValidatedEventAPIGatewayProxyEvent<void> = async (event) => {
 
     const id = event.pathParameters.id
 
     await todoService.delete(id)
 
     return {
-        statusCode: 204
+        statusCode: 204,
+        body: null
     }
 
 };
--- src/functions/listTodos/handler.ts
+++ src/functions/listTodos/handler.ts
@@ -1,11 +1,11 @@
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
-import { Any } from 'json-schema-to-ts/lib/meta-types';
+import { TodoItem } from 'src/models/TodoItem';
 import TodoService from 'src/services/TodoService';
 
 const todoService = new TodoService()
 
-const listTodos: ValidatedEventAPIGatewayProxyEvent<Any> = async () => {
+const listTodos: ValidatedEventAPIGatewayProxyEvent<TodoItem[]> = async () => {
 
     const list = await todoService.list()
 
